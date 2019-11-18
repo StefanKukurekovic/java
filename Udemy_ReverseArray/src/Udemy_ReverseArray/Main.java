bd8a5d8a336078378b0bd9190a50841ae1645fcf
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2019-11-19 00:20:09 +0100

    update

--- Udemy_ReverseArray/src/Udemy_ReverseArray/Main.java
+++ Udemy_ReverseArray/src/Udemy_ReverseArray/Main.java
@@ -1,10 +1,13 @@
 package Udemy_ReverseArray;
+import java.util.Scanner;
 
 public class Main {
-
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
 
 }
