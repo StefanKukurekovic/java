8d971a5704e4747c7ed811c931205916d8ac7310
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2019-11-20 16:14:42 +0100

    Update Reverse

--- Udemy_ReverseArray/src/Udemy_ReverseArray/Main.java
+++ Udemy_ReverseArray/src/Udemy_ReverseArray/Main.java
@@ -6,8 +6,42 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[] newArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+		
+		reverse(newArray);
 
 	}
 	
+	public static void reverse(int[] reverseArray)
+	{
+		int temp = 0;
+		System.out.println("Non reversed array:");
+		
+		printArray(reverseArray);
+		
+		System.out.println("\nReversed array:");
+		for(int i = 1; i <= reverseArray.length-1; i++)
+		{
+			if((double)i-1 == Math.ceil((double)reverseArray.length/2.0))
+			{
+				break;
+			}else {
+				temp = reverseArray[i-1];
+				reverseArray[i-1] = reverseArray[reverseArray.length-i];
+				reverseArray[reverseArray.length-i] = temp;
+			}
+		}
+		
+		printArray(reverseArray);
+	}
+	
+	public static void printArray(int[] someArray)
+	{
+		for(int i = 0; i < someArray.length; i++)
+		{
+			System.out.print("[" + i + "]-->" + someArray[i] + "; ");
+		}
+	}
 
 }
