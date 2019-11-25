f1a5047359cb27f5b7945101f05123abcb48d7bf
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2019-11-25 22:17:49 +0100

    new version

--- Udemy_ArrayList/src/Udemy_ArrayList/Main.java
+++ Udemy_ArrayList/src/Udemy_ArrayList/Main.java
@@ -3,14 +3,50 @@ package Udemy_ArrayList;
 import java.util.Scanner;
 
 public class Main {
+	
+	private static Scanner s = new Scanner(System.in);
+	private static int[] baseData = new int[10];
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter 10 integers:");
+		getInput();
+		printArray(baseData);
+		resizeArray();
+		System.out.println("Enter 12 integers:");
+		getInput();
+		printArray(baseData);
 		
-		Scanner sc = new Scanner(System.in);
+		s.close();
 		
-		sc.close();
+	}
+	
+	private static void getInput()
+	{
+		for(int i = 0; i < baseData.length; i++)
+		{
+			baseData[i] = s.nextInt();
+		}
+	}
+	
+	private static void printArray(int[] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	private static void resizeArray()
+	{
+		int[] original = baseData;
 		
+		baseData = new int[12];
+		
+		for(int i = 0; i<original.length; i++)
+		{
+			baseData[i] = original[i];
+		}
 	}
 
 }
