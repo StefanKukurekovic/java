58a99ce758c358508151417f9f542ba5404cf03c
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2019-11-27 12:06:44 +0100

    InsertionSort

---
+++ InsertionSort/src/InsertionSort/Main.java
@@ -0,0 +1,78 @@
+package InsertionSort;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {6,1,2,7,33,62,4,72,47,8,66,100,95};
+		
+		System.out.println("Bubble sort:");
+		bubble(arr);
+		print(arr);
+		System.out.println("\nInsertion sort:");
+		insertion(arr);
+		print(arr);
+
+	}
+	
+	public static void swap(int arr[], int i, int j)
+	{
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	public static void bubble(int numbers[]) {
+		 /* i actually counts the number of runs*/
+		 for (int i=0;i<numbers.length-1;i++){
+			 /*the last i positions are correct*/
+			 for (int j=1;j<numbers.length-i;j++)
+				 /*swap neighbors, if required*/
+				 if (numbers[j-1]>numbers[j])
+					 swap(numbers,j-1,j);
+			 }
+	}
+	
+	public static void bubbleFast(int numbers[]) {
+		 boolean swapped;
+		 int i=0;
+		 do {
+			 swapped = false;
+			 for (int j = 1; j < numbers.length - i; j++)
+				 if (numbers[j - 1] > numbers[j]) {
+					 swap(numbers, j - 1, j);
+					 swapped = true;
+				 }
+			 i++;
+		 } while (swapped);
+	}
+	
+	public static void insertion(int numbers[]) {
+
+		 /* i is the first idx of unsorted partition*/
+		 for (int i=1; i<numbers.length; i++) {
+			 int j=i;
+			 /*tmp holds value of next element to insert*/
+			 int tmp=numbers[i];
+			 /*find correct position for tmp*/
+			 while (j>0 && tmp<numbers[j-1]) {
+				 /*on the way there shift right*/
+				 numbers[j]=numbers[j-1];
+				 j--;
+			 }
+			 numbers[j]=tmp;
+		 }
+		}
+
+
+	
+	public static void print(int arr[])
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+
+}
