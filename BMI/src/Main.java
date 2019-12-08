46aaedf2d5a766f3fa8271c7674eb19c55e5d558
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2019-12-08 20:35:12 +0100

    Small update

--- BMI/src/Main.java
+++ BMI/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// Scanner function
 		Scanner sc = new Scanner(System.in);
 		
 		double weight, height, bmi;
