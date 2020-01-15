2d0afa8077c34bccd3f571640e36e0e4c937a390
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2020-01-16 00:24:10 +0100

    excercise

--- test/src/test/LendItemArrayList.java
+++ test/src/test/LendItemArrayList.java
@@ -11,5 +11,22 @@ public class LendItemArrayList {
 //	 LendItem[] lendItems = new LendItem[INITIAL_SIZE];	 
 //	 List<String> lendItems = Arrays.asList(new String[INITIAL_SIZE]);
 	 
-	 ArrayList<String> lendItems = new ArrayList<String>();
+	 List<LendItem> lendItems = new ArrayList<LendItem>(INITIAL_SIZE);
+	 
+	 public void addLendItems(LendItem item) {
+		 lendItems.add(item);
+	 }
+	 
+	 public void printLendItems() {
+		 for(int i = 0; i < lendItems.size(); i++)
+		 {
+			 System.out.println((i+1) + ". " + lendItems.get(i).description + " " + lendItems.get(i).owner + " " + lendItems.get(i).lender);
+		 }
+	 }
+	 
+	 public void removeLendItem(int position) {
+		 LendItem theItem = lendItems.get(position);
+		 lendItems.remove(position);
+	 }
+	 
 }
--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -7,46 +7,71 @@ import java.util.Scanner;
 
 public class Main {
 		
+		private static Scanner sc = new Scanner(System.in);
+		
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		LendItem item = new LendItem();
 		LendItemArrayList list = new LendItemArrayList();
+		LendItem item = new LendItem();
+		
+		
 		
 //		Menu();
 //		item = scanLendItem(sc);
-		add(list, scanLendItem(sc));
-		System.out.printf("\n%s", lendItemString(item));
+//		add(list, item);
+//		System.out.printf("\n%s", lendItemString(item));
 //		printList(list);
-		printList(list);
+//		printList(list);
 		
-//		String input = "";
-//		do {
-//        	Menu();
-//        	input = sc.nextLine();
+		String input = "";
+		do {
+        	Menu();
+        	input = sc.nextLine();
 //            //System.out.printf("enter opcode (9 for usage): ");
 //            // input = sc.nextInt();
-//        	switch(input) {
-//        	case "1":
+        	switch(input) {
+        	case "1":
+        		list.printLendItems();
 //        		System.out.printf("\n%s", lendItemString(item));
-//        		break;
-//        	case "2":
-//        		add(list, item);
-//        		break;
-//        	case "0":
-//        		break;
-//        	default:
-//        		System.out.println("This is the end.");
-//        		break;            
-//            }
+        		break;
+        	case "2":
+        		addItem(list, scanLendItem(sc));
+        		break;
+        	case "3":
+        		System.out.print("enter ID of LendItem to be removed: ");
+        		int itemNo = sc.nextInt();
+        		sc.nextLine();
+        		removeLendItem(list, itemNo);
+        		break;
+        	case "0":
+        		break;
+        	default:
+        		System.out.println("This is the end.");
+        		break;            
+            }
 //
-//        } while (input != null && !input.equals(""));
-//        sc.close();
-//        System.out.printf("Thank you for using LendApp.\n");
+        } while (input != "0" && !input.equals(""));
+        sc.close();
+        System.out.printf("Thank you for using LendApp.\n");
     }
 		
 		
-		
+	public static void addItem(LendItemArrayList list, LendItem p) {
+		list.addLendItems(p);
+	}
 	
+	public static void removeLendItem(LendItemArrayList list, int n)
+	{
+		list.removeLendItem(n-1);
+	}
+	
+	public static void list(LendItemArrayList list, int n)
+	{
+		for(int i = 0; i < list.length; i++) {
+			
+		}
+	}
+	
+	list.printLendItems();
 	
 	
 	// Displays start menu at the beginning 
@@ -62,13 +87,13 @@ public class Main {
     }
 	
 	// Adds new item to the program
-	public static void add(LendItemArrayList list, LendItem p) {
-		
-//		if(isFull(list))
-//			resize(list);
-		
-		list.lendItems[list.next++] = p; 	
-	}
+//	public static void add(LendItemArrayList list, LendItem p) {
+//		
+////		if(isFull(list))
+////			resize(list);
+//		
+//		list.lendItems[list.next++] = p; 	
+//	}
 	
 	// Scans new items
 	public static LendItem scanLendItem(Scanner sc) {
@@ -97,25 +122,25 @@ public class Main {
 	}
 	
 	// Prints out the array list
-	public static void printList(LendItemArrayList list) {
-//		System.out.println("List is Empty: " + isEmpty(list));
-//		System.out.println("List is Full:  " + isFull(list));
-		for(int i = 0; i < list.lendItems.length; i++) {
-			if(i >= list.next) {
-				System.out.print("_ ");
-				continue;
-			}
-			
-			System.out.print(list.lendItems[i] + " ");
-		}
-		System.out.println();
-		System.out.println();
-	}
+//	public static void printList(LendItemArrayList list) {
+////		System.out.println("List is Empty: " + isEmpty(list));
+////		System.out.println("List is Full:  " + isFull(list));
+//		for(int i = 0; i < list.length(); i++) {
+//			if(i >= list.next) {
+//				System.out.print("_ ");
+//				continue;
+//			}
+//			
+//			System.out.print(list.lendItems[i] + " ");
+//		}
+//		System.out.println();
+//		System.out.println();
+//	}
 	
 	
-	public static String lendItemString(LendItem it /*, int format*/) {
-//        switch (format) {
-//        case 1:
+	public static String lendItemString(LendItem it, int format) {
+        switch (format) {
+        case 1:
             return String.format("%-15.15s %-10.10s %s", /* %-10.10s", lendItemHeadings(format), it.ID, */ it.description, it.lender, /*dateString(it.lendDate),
                     dateString(it.returnDate),*/ it.owner);
 //        case 2:
@@ -123,7 +148,6 @@ public class Main {
 //        default:
 //            return String.format("%s\n\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"", lendItemHeadings(format), it.ID, it.description, it.lender,
 //                    dateString(it.lendDate), dateString(it.returnDate), it.owner);
-//        }
 
     }
 
