e6d770d8e3bf3aedefe3761152747f031317518c
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2020-01-15 21:00:37 +0100

    excercise

--- test/src/test/LendItemArrayList.java
+++ test/src/test/LendItemArrayList.java
@@ -1,9 +1,15 @@
 package test;
 import test.LendItem;
+import java.util.List;
+import java.util.ArrayList;
+
 public class LendItemArrayList {
 	 int INITIAL_SIZE = 20;
 	 boolean resizeable = false;
 	 int next = 0;
 	 
-	 LendItem[] lendItems = new LendItem[INITIAL_SIZE];	 
+//	 LendItem[] lendItems = new LendItem[INITIAL_SIZE];	 
+//	 List<String> lendItems = Arrays.asList(new String[INITIAL_SIZE]);
+	 
+	 ArrayList<String> lendItems = new ArrayList<String>();
 }
